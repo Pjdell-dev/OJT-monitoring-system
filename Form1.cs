43fdf340ea2c07796e1919f452bc8bb49c6530d4
gdnTUA921 <giancarlodnonato@tua.edu.ph>
2024-10-16 14:40:49 +0800

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+namespace OJT_Monitoring
+{
+    using MySql.Data.MySqlClient; // Do not forget this part
+    using Org.BouncyCastle.Crypto.Engines;
+    using System.Data;
+    using System.Data.Common;
+
+    public partial class LogIn : Form
+    {
+        static string server = "localhost";
+        static string port = "3306";
+        static string username = "root";
+        static string password = "";
+        static string database = "ojt";
+
+        MySqlConnection dbConn = new MySqlConnection(
+            "server=" + server + ";" +
+            "port=" + port + ";" +
+            "username=" + username + ";" +
+            "password=" + password + ";" +
+            "database=" + database + ";");
+
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dbConn.Open();
+
+            string dbQuery = "SELECT * FROM admin WHERE username = @username AND password = @password"; // actual SQL
+            MySqlCommand dbCmd = new MySqlCommand(dbQuery, dbConn); // commands from c# to MySQL
+
+            dbCmd.Parameters.AddWithValue("@username", logInUsername.Text);
+            dbCmd.Parameters.AddWithValue("@password", logInpassword.Text);
+            MySqlDataReader dbReader = dbCmd.ExecuteReader(); // to fetch the data from database
+
+            String userName;
+            String password;
+            String accountType;
+
+            while (dbReader.Read())
+            {
+                userName = dbReader.GetString("username");
+                password = dbReader.GetString("password");
+                accountType = dbReader.GetString("account_type");
+            }
+
+            if (accountType == "Admin")
+            {
+                MessageBox.Show("Login Succcessfully");
+                this.Hide();
+                string LoggedUsername = logInUsername.Text;
+
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid.");
+            }
+
+
+        }
+    }
+}
