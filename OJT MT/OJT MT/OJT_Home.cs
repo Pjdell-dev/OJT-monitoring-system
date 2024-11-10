7511c06f8fac3b2012699f92fd99762d4a85a047
gdnTUA921 <giancarlodnonato@tua.edu.ph>
2024-11-10 23:18:56 +0800

    Add files via upload

---
+++ OJT MT/OJT MT/OJT_Home.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OJT_MT
+{ 
+
+    public partial class OJT_Home : Form
+    {
+        MainForm _mainform;
+
+        public OJT_Home(MainForm mainform)
+        {
+            this._mainform = mainform;
+            InitializeComponent();
+            loadHomeAsync();
+            
+        }
+
+        public async Task loadHomeAsync()
+        {
+            string accountType = _mainform.accountType;
+            string accountID = _mainform.accountID;
+            using var dbHelper = new DatabaseHelper();
+
+            if (accountType == "admin")
+            {
+                String dbQuery = "SELECT first_name FROM administrators WHERE user_id = " + accountID;
+
+                using var reader = await dbHelper.ExecuteReaderAsync(dbQuery);
+
+                if (reader.Read())
+                {
+                    String firstName = reader.GetString("first_name");
+
+                    welcomeLabel.Text = "Welcome " + firstName + "!";
+                }
+            }
+
+            else if (accountType == "supervisor")
+            {
+                String dbQuery = "SELECT first_name FROM supervisors WHERE user_id = " + accountID;
+
+                using var reader = await dbHelper.ExecuteReaderAsync(dbQuery);
+
+                if (reader.Read())
+                {
+                    String firstName = reader.GetString("first_name");
+
+                    welcomeLabel.Text = "Welcome " + firstName + "!";
+                }
+            }
+        }
+    }
+}
---
+++ OJT MT/OJT MT/qrGen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OJT_MT
+{
+    public partial class qrGen : Form
+    {
+        MainForm _mainForm;
+        private string _accountType;
+        private int? _selectedSupervisorId = null;
+        private bool _isEmailValid = false;
+        private bool _isPasswordValid = false;
+
+        public qrGen(MainForm mainForm)
+        {
+            _mainForm = mainForm;
+            InitializeComponent();
+        }
+
+
+        private static Random random = new Random();
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void btnGenerateTimeIn_Click(object sender, EventArgs e)
+        {
+            int length = 15;
+            String textGen = RandomString(length);
+            txtQR.Text = textGen;
+
+            QRCoder.QRCodeGenerator QG = new QRCoder.QRCodeGenerator();
+            var MyData = QG.CreateQrCode(txtQR.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(MyData);
+            pictureBox1.Image = code.GetGraphic(50);
+        }
+
+        private void btnGenerateTimeOut_Click(object sender, EventArgs e)
+        {
+            int length = 15;
+            String textGen = RandomString(length);
+            txtQR.Text = textGen;
+
+            QRCoder.QRCodeGenerator QG = new QRCoder.QRCodeGenerator();
+            var MyData = QG.CreateQrCode(txtQR.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(MyData);
+            pictureBox1.Image = code.GetGraphic(50);
+        }
+    }
+}
